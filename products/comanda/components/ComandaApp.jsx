d7433a0e6710498e0d3723bd7657c259913f5c1e
grangabyk-debug <grangabyk@gmail.com>
2026-08-18 02:21:29 -0300

    feat(comanda): mount workstation permission guard

--- products/comanda/components/ComandaApp.jsx
+++ products/comanda/components/ComandaApp.jsx
@@ -7,7 +7,8 @@ import ComandaSessionUX from "./ComandaSessionUX";
 import ComandaKitchenKDS from "./ComandaKitchenKDS";
 import ComandaWaiterCash from "./ComandaWaiterCash";
 import ComandaSectorPlanner from "./ComandaSectorPlanner";
+import ComandaRoleGuard from "./ComandaRoleGuard";
 
 export default function ComandaApp(){
-  return <ComandaDashboardChrome><ComandaNavigationGuard/><ComandaSessionUX/><ComandaKitchenKDS/><ComandaWaiterCash/><ComandaSectorPlanner/><ComandaMenuLayer><ComandaFinal/></ComandaMenuLayer></ComandaDashboardChrome>;
+  return <ComandaDashboardChrome><ComandaNavigationGuard/><ComandaSessionUX/><ComandaRoleGuard/><ComandaKitchenKDS/><ComandaWaiterCash/><ComandaSectorPlanner/><ComandaMenuLayer><ComandaFinal/></ComandaMenuLayer></ComandaDashboardChrome>;
 }
