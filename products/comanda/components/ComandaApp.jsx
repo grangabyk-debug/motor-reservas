e3268ecf5cc068fe9822c59856e62c174be33b89
grangabyk-debug <grangabyk@gmail.com>
2026-08-18 02:06:36 -0300

    feat(comanda): mount workstation-aware session UX

--- products/comanda/components/ComandaApp.jsx
+++ products/comanda/components/ComandaApp.jsx
@@ -3,7 +3,8 @@ import ComandaFinal from "./ComandaFinal";
 import ComandaDashboardChrome from "./ComandaDashboardChrome";
 import ComandaMenuLayer from "./ComandaMenuLayer";
 import ComandaNavigationGuard from "./ComandaNavigationGuard";
+import ComandaSessionUX from "./ComandaSessionUX";
 
 export default function ComandaApp(){
-  return <ComandaDashboardChrome><ComandaNavigationGuard/><ComandaMenuLayer><ComandaFinal/></ComandaMenuLayer></ComandaDashboardChrome>;
+  return <ComandaDashboardChrome><ComandaNavigationGuard/><ComandaSessionUX/><ComandaMenuLayer><ComandaFinal/></ComandaMenuLayer></ComandaDashboardChrome>;
 }
