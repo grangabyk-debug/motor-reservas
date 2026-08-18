0192f1d78750e4a15684fba73fd882ea1157cda9
grangabyk-debug <grangabyk@gmail.com>
2026-08-18 02:49:31 -0300

    feat(comanda): integrate reports hub

--- products/comanda/components/ComandaApp.jsx
+++ products/comanda/components/ComandaApp.jsx
@@ -14,7 +14,8 @@ import ComandaUserManager from "./ComandaUserManager";
 import ComandaWorkstationManager from "./ComandaWorkstationManager";
 import ComandaPrinterManager from "./ComandaPrinterManager";
 import ComandaPrincipalHome from "./ComandaPrincipalHome";
+import ComandaReportsHub from "./ComandaReportsHub";
 
 export default function ComandaApp(){
-  return <ComandaAccessGate><ComandaDashboardChrome><ComandaNavigationGuard/><ComandaSessionUX/><ComandaRoleGuard/><ComandaPrincipalHome/><ComandaKitchenKDS/><ComandaWaiterCash/><ComandaSectorPlanner/><ComandaUserManager/><ComandaStaffManager/><ComandaWorkstationManager/><ComandaPrinterManager/><ComandaMenuLayer><ComandaFinal/></ComandaMenuLayer></ComandaDashboardChrome></ComandaAccessGate>;
+  return <ComandaAccessGate><ComandaDashboardChrome><ComandaNavigationGuard/><ComandaSessionUX/><ComandaRoleGuard/><ComandaPrincipalHome/><ComandaReportsHub/><ComandaKitchenKDS/><ComandaWaiterCash/><ComandaSectorPlanner/><ComandaUserManager/><ComandaStaffManager/><ComandaWorkstationManager/><ComandaPrinterManager/><ComandaMenuLayer><ComandaFinal/></ComandaMenuLayer></ComandaDashboardChrome></ComandaAccessGate>;
 }
