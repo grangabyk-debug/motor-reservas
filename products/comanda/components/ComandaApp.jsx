ec6a1ca07f11edbba54832008fbbf24a872a4211
grangabyk-debug <grangabyk@gmail.com>
2026-08-18 02:46:11 -0300

    feat(comanda): integrate user access manager

--- products/comanda/components/ComandaApp.jsx
+++ products/comanda/components/ComandaApp.jsx
@@ -10,9 +10,10 @@ import ComandaSectorPlanner from "./ComandaSectorPlanner";
 import ComandaRoleGuard from "./ComandaRoleGuard";
 import ComandaAccessGate from "./ComandaAccessGate";
 import ComandaStaffManager from "./ComandaStaffManager";
+import ComandaUserManager from "./ComandaUserManager";
 import ComandaWorkstationManager from "./ComandaWorkstationManager";
 import ComandaPrincipalHome from "./ComandaPrincipalHome";
 
 export default function ComandaApp(){
-  return <ComandaAccessGate><ComandaDashboardChrome><ComandaNavigationGuard/><ComandaSessionUX/><ComandaRoleGuard/><ComandaPrincipalHome/><ComandaKitchenKDS/><ComandaWaiterCash/><ComandaSectorPlanner/><ComandaStaffManager/><ComandaWorkstationManager/><ComandaMenuLayer><ComandaFinal/></ComandaMenuLayer></ComandaDashboardChrome></ComandaAccessGate>;
+  return <ComandaAccessGate><ComandaDashboardChrome><ComandaNavigationGuard/><ComandaSessionUX/><ComandaRoleGuard/><ComandaPrincipalHome/><ComandaKitchenKDS/><ComandaWaiterCash/><ComandaSectorPlanner/><ComandaUserManager/><ComandaStaffManager/><ComandaWorkstationManager/><ComandaMenuLayer><ComandaFinal/></ComandaMenuLayer></ComandaDashboardChrome></ComandaAccessGate>;
 }
