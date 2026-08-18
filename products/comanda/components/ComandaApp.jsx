3ed7eeec6e3be3a7fa18859d331c92e6eadb8cc1
grangabyk-debug <grangabyk@gmail.com>
2026-08-18 02:12:03 -0300

    feat(comanda): mount limited waiter cash controls

--- products/comanda/components/ComandaApp.jsx
+++ products/comanda/components/ComandaApp.jsx
@@ -5,7 +5,8 @@ import ComandaMenuLayer from "./ComandaMenuLayer";
 import ComandaNavigationGuard from "./ComandaNavigationGuard";
 import ComandaSessionUX from "./ComandaSessionUX";
 import ComandaKitchenKDS from "./ComandaKitchenKDS";
+import ComandaWaiterCash from "./ComandaWaiterCash";
 
 export default function ComandaApp(){
-  return <ComandaDashboardChrome><ComandaNavigationGuard/><ComandaSessionUX/><ComandaKitchenKDS/><ComandaMenuLayer><ComandaFinal/></ComandaMenuLayer></ComandaDashboardChrome>;
+  return <ComandaDashboardChrome><ComandaNavigationGuard/><ComandaSessionUX/><ComandaKitchenKDS/><ComandaWaiterCash/><ComandaMenuLayer><ComandaFinal/></ComandaMenuLayer></ComandaDashboardChrome>;
 }
