aa63cf095bfab5c284dd2bac021f48c977952121
grangabyk-debug <grangabyk@gmail.com>
2026-09-01 22:46:13 -0300

    style: activate final PMS module normalization

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -2,6 +2,7 @@ import HotelOSV2 from"./HotelOSV2"
 import StaticCaretGuard from"../components/StaticCaretGuard"
 import MercadoPagoOAuthBridge from"./components/MercadoPagoOAuthBridge"
 import"./pms-unified.css"
+import"./pms-unified-modules.css"
 
 export default function DashboardPage(){
   return <><StaticCaretGuard/><MercadoPagoOAuthBridge/><div className="hlHotelgest"><HotelOSV2/></div></>
