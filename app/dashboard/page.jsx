385f4009f49f503150f14a5bf10228dbe49659dc
grangabyk-debug <grangabyk@gmail.com>
2026-09-02 17:23:28 -0300

    load final planning responsive alignment fixes

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -25,6 +25,7 @@ import"./planning-quick-modal.css"
 import"./planning-quick-modal-v2.css"
 import"./planning-hover-placement.css"
 import"./planning-toolbar-rack-final.css"
+import"./planning-toolbar-responsive-fix.css"
 
 export const metadata={
   title:"Habitación Llena",
