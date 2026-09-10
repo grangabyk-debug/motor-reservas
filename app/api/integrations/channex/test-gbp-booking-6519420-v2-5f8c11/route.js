254c94ad0a0646c01e55ce2bd1b9d667ac6ba6d5
grangabyk-debug <grangabyk@gmail.com>
2026-09-10 19:19:41 -0300

    fix(channex): corregir import del TEST GBP v2

--- app/api/integrations/channex/test-gbp-booking-6519420-v2-5f8c11/route.js
+++ app/api/integrations/channex/test-gbp-booking-6519420-v2-5f8c11/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server"
-import { adminSupabase, channexMode, channexRequest } from "../../../../../../lib/channexServer"
+import { adminSupabase, channexMode, channexRequest } from "../../../../../lib/channexServer"
 
 export const runtime = "nodejs"
 export const dynamic = "force-dynamic"
