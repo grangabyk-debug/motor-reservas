3227f43842f69a7f9f4586f2845f71c8d869f803
grangabyk-debug <grangabyk@gmail.com>
2026-09-15 22:43:05 -0300

    Activar barra de mando Hotel OS en Reservas

--- app/pms-next/features/reservations/ReservationsWorkspace.jsx
+++ app/pms-next/features/reservations/ReservationsWorkspace.jsx
@@ -5,15 +5,16 @@ import ReservationsWorkspaceBase from"./ReservationsWorkspaceBase"
 import ReservationDuplicateWatch from"./ReservationDuplicateWatch"
 import GuestAttentionPanel from"./GuestAttentionPanel"
 import GuestDuplicateWatch from"./GuestDuplicateWatch"
+import hotel from"./reservationsHotelOs.module.css"
 
 export default function ReservationsWorkspace(props){
   const searchParams=useSearchParams(),recordOpen=Boolean(searchParams.get("reservation"))
-  return <>
+  return <div className={hotel.reservationsOs}>
     {!recordOpen?<>
       <GuestAttentionPanel propertyId={props.propertyId}/>
       <GuestDuplicateWatch propertyId={props.propertyId}/>
       <ReservationDuplicateWatch propertyId={props.propertyId}/>
     </>:null}
     <ReservationsWorkspaceBase {...props}/>
-  </>
+  </div>
 }
