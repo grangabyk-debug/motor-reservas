91e95513f16304b14b3de598e27fe2f8dc38b8b2
grangabyk-debug <grangabyk@gmail.com>
2026-09-17 01:32:55 -0300

    Restaurar ficha directamente al volver de un cobro cancelado

--- app/pms-next/features/reservations/ReservationsWorkspace.jsx
+++ app/pms-next/features/reservations/ReservationsWorkspace.jsx
@@ -8,13 +8,16 @@ import GuestDuplicateWatch from"./GuestDuplicateWatch"
 import hotel from"./reservationsHotelOs.module.css"
 
 export default function ReservationsWorkspace(props){
-  const searchParams=useSearchParams(),recordOpen=Boolean(searchParams.get("reservation"))
+  const searchParams=useSearchParams()
+  const urlReservationId=Number(searchParams.get("reservation"))||null
+  const recordOpen=Boolean(urlReservationId)
+  const effectiveFocusId=props.focusReservationId??urlReservationId
   return <div className={hotel.reservationsOs}>
     {!recordOpen?<>
       <GuestAttentionPanel propertyId={props.propertyId}/>
       <GuestDuplicateWatch propertyId={props.propertyId}/>
       <ReservationDuplicateWatch propertyId={props.propertyId}/>
     </>:null}
-    <ReservationsWorkspaceBase {...props}/>
+    <ReservationsWorkspaceBase {...props} focusReservationId={effectiveFocusId}/>
   </div>
 }
