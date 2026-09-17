a2a7e7fea1ed604474fdb2cc19b031b216637989
grangabyk-debug <grangabyk@gmail.com>
2026-09-17 01:25:47 -0300

    Volver a la ficha al cancelar cobro desde reserva

--- app/pms-next/features/finance/ReservationPaymentPanelMultiCurrency.jsx
+++ app/pms-next/features/finance/ReservationPaymentPanelMultiCurrency.jsx
@@ -1,1 +1,14 @@
-export{default}from"../../../pms-shared/features/finance/ReservationPaymentPanelMultiCurrencyV2"
+"use client"
+
+import ReservationPaymentPanelMultiCurrencyV2 from"../../../pms-shared/features/finance/ReservationPaymentPanelMultiCurrencyV2"
+
+export default function ReservationPaymentPanelMultiCurrency(props){
+  function closePayment(){
+    const url=typeof window!=="undefined"?new URL(window.location.href):null
+    const openedFromReservation=Boolean(url?.searchParams.get("cash_reservation"))
+    props.onClose?.()
+    if(openedFromReservation&&typeof window!=="undefined")requestAnimationFrame(()=>window.history.back())
+  }
+
+  return <ReservationPaymentPanelMultiCurrencyV2 {...props} onClose={closePayment}/>
+}
